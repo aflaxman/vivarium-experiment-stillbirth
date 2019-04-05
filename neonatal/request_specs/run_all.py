64a7dfd56a91ba279a7a784b5bc64ab0bca6abb8
Cody <chorst@uw.edu>
2019-04-05 12:35:55 -0700

    add template spec and script to run them all

---
+++ neonatal/request_specs/run_all.py
@@ -0,0 +1,63 @@
+import subprocess
+
+"""
+Locations:
+    India
+    Ethiopia
+    
+    Uttar Pradesh
+    Kerala
+    Bihar
+    
+    Sub-saharan Africa Super-region
+    South Asia Region
+    Low SDI
+    Low-middle SDI
+    Middle SDI
+    High-middle SDI
+    High SDI
+"""
+
+
+def run(locations):
+    """Generates specs for different locations and builds artifacts with them."""
+    for loc in locations:
+        name = loc
+        fname = loc.replace(' ', '_')
+
+        with open("template.yaml") as f:
+            template_spec = f.read()
+
+        template_spec.format(name=name, fname=fname)
+
+        with open(f"{fname}.yaml") as f:
+            f.write(template_spec)
+
+        subprocess.call(f"build_artifact -v {fname}.yaml")
+
+
+def nat():
+    locations = ['Ethiopia', 'India']
+    run(locations)
+
+
+def supernat():
+    locations = ['Sub-saharan Africa', 'South Asia',
+                 'Low SDI', 'Low-middle SDI', 'Middle SDI', 'High-middle SDI', 'High SDI']
+    run(locations)
+
+
+def subnat():
+    locations = ['Uttar Pradesh', 'Kerala', 'Bihar']
+    run(locations)
+
+
+if __name__ == "__main__":
+    import sys
+    which = sys.argv[1]
+    if sys.argv[1] == 'subnat':
+        subnat()
+    elif sys.argv[1] == 'supernat':
+        supernat()
+    else:
+        nat()
