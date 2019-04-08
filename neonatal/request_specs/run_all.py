87975a331f382795ab8491724a7e34d3d2a20537
Cody <chorst@uw.edu>
2019-04-07 23:12:28 -0700

    use the cluster

--- neonatal/request_specs/run_all.py
+++ neonatal/request_specs/run_all.py
@@ -1,3 +1,5 @@
+import shutil
+import os
 import subprocess
 
 """
@@ -19,45 +21,63 @@ Locations:
 """
 
 
-def run(locations, level):
-    """Generates specs for different locations and builds artifacts with them."""
-    for loc in locations:
-        name = loc
-        fname = loc.replace(' ', '_')
+def run(loc, level):
+    """Generates specs for a location and build artifacts with them."""
+    name = loc
+    fname = loc.replace(' ', '_')
 
-        with open("template.yaml") as f:
-            template_spec = f.read()
+    with open("template.yaml") as f:
+        template_spec = f.read()
 
-        template_spec = template_spec.format(name=name, fname=fname)
+    template_spec = template_spec.format(name=name, fname=fname)
 
-        with open(f"{fname}_{level}.yaml", 'w') as f:
-            f.write(template_spec)
+    with open(f"{fname}_{level}.yaml", 'w') as f:
+        f.write(template_spec)
 
-        subprocess.call(["build_artifact", "-v", '-o', './', f"{fname}_{level}.yaml"])
-
-
-def nat():
-    locations = ['Ethiopia', 'India']
-    run(locations, 'nat')
+    subprocess.call(["build_artifact", "-v", '-o', './', f"{fname}_{level}.yaml"])
 
 
-def supernat():
-    locations = ['Sub-Saharan Africa', 'South Asia',
-                 'Low SDI', 'Low-middle SDI', 'Middle SDI', 'High-middle SDI', 'High SDI']
-    run(locations, 'supernat')
+# def nat():
+#     locations = ['Ethiopia', 'India']
+#     run(locations, 'nat')
+#
+#
+# def supernat():
+#     locations = ['Sub-Saharan Africa', 'South Asia',
+#                  'Low SDI', 'Low-middle SDI', 'Middle SDI', 'High-middle SDI', 'High SDI']
+#     run(locations, 'supernat')
+#
+#
+# def subnat():
+#     locations = ['Uttar Pradesh, Rural', 'Uttar Pradesh, Urban', 'Kerala, Rural', 'Kerala, Urban', 'Bihar, Rural',
+#                  'Bihar, Urban']
+#     run(locations, 'subnat')
 
 
-def subnat():
-    locations = ['Uttar Pradesh, Rural', 'Uttar Pradesh, Urban', 'Kerala, Rural', 'Kerala, Urban', 'Bihar, Rural',
-                 'Bihar, Urban']
-    run(locations, 'subnat')
+def launch():
+    import drmaa
+    locations = {'national': ['Ethiopia', 'India'],
+                 'supernational': ['Sub-Saharan Africa', 'South Asia', 'Low SDI', 'Low-middle SDI', 'Middle SDI',
+                                   'High-middle SDI', 'High SDI'],
+                 'subnational': ['Uttar Pradesh, Rural', 'Uttar Pradesh, Urban', 'Kerala, Rural', 'Kerala, Urban',
+                                 'Bihar, Rural', 'Bihar, Urban']}
+
+    with drmaa.Session() as s:
+        jt = s.createJobTemplate()
+        jt.workingDirectory = os.getcwd()
+        jt.remoteCommand = shutil.which('python')
+        for level, sub_locations in locations.items():
+            for loc in sub_locations:
+                loc = loc.replace(' ', '_')  # worried about spaces ruining things
+                jt.nativeSpecification = f'-w n -q all.q -l m_mem_free=20G -N {loc} -l fthread=1 -P proj_cost_effect'
+                jt.args = ['run_all.py', loc, level]
+                s.runJob(jt)
+        s.deleteJobTemplate(jt)
 
 
 if __name__ == "__main__":
     import sys
-    if sys.argv[1] == 'subnat':
-        subnat()
-    elif sys.argv[1] == 'supernat':
-        supernat()
-    else:
-        nat()
+    loc = sys.argv[1]
+    loc = loc.replace('_', ' ')
+    level = sys.argv[2]
+    run(loc, level)
